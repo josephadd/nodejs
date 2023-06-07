2d9ec514cb4a3ae5484ca9c1fb2801574375d201
nx-joseph-adeabah <85238365+josephadd@users.noreply.github.com>
2023-06-07 08:03:47 +0000

    feat: added server to app

---
+++ config.js
@@ -0,0 +1,14 @@
+// Replace with your actual Twilio phone number
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+
+// Replace with your actual Twilio auth token
+const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+
+// Replace with your actual Twilio account SID
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+
+module.exports = {
+  twilioPhoneNumber,
+  twilioAuthToken,
+  twilioAccountSid
+};
