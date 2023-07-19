8be542dd6d207e46b0dd179e99b91aa69392e5fb
nx-joseph-adeabah <85238365+josephadd@users.noreply.github.com>
2023-07-19 20:56:50 +0000

    added update

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const WebSocket = require("ws");
 const express = require("express");
-const cors = require("cors");
-const http = require("http");
 const app = express();
-app.use(cors()); // Enable CORS for all routes
-app.use(express.json());
-const server = http.createServer(app);
+const server = require("http").createServer(app);
 const wss = new WebSocket.Server({ server });
 const speech = require("@google-cloud/speech");
 require("dotenv").config();
 
+const {
+  removeDuplicateKeywords
+} = require("./Helper");
 const client = new speech.SpeechClient({
   keyFilename: "../server/routes/geohilfev1-1b4d84da44a9.json",
 });
+
 
 // Configure Transcription Request
 const request = {
@@ -20,9 +20,6 @@ const request = {
     encoding: "MULAW",
     sampleRateHertz: 8000,
     languageCode: "en-GB",
-    enableAutomaticPunctuation: true, // Enable automatic punctuation
-    enableWordConfidence: true, // Enable word-level confidence
-    enableWordTimeOffsets: true, // Enable word-level time offsets
   },
   interimResults: true,
 };
@@ -44,43 +41,20 @@ wss.on("connection", function connection(ws) {
           .on("error", console.error)
           .on("data", async (data) => {
             const caller = "Connected...";
-            // Make a streaming request to the extraction endpoint
-            const streamingReq = http.request(
-              {
-                host: "localhost",
-                port: 8000,
-                path: "/extract",
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              },
-              (res) => {
-                res.on("data", (chunk) => {
-                  const response = JSON.parse(chunk.toString());
-                  console.log(response.keywords);
-                  wss.clients.forEach((client) => {
-                    if (client.readyState === WebSocket.OPEN) {
-                      client.send(
-                        JSON.stringify({
-                          event: "interim-transcription",
-                          keywords: response.keywords,
-                        })
-                      );
-                    }
-                  });
-                });
+            console.log(data.results[0].alternatives[0].transcript);
+            
+            // Process the data as required (removed axios post request)
+            
+            wss.clients.forEach((client) => {
+              if (client.readyState === WebSocket.OPEN) {
+                client.send(
+                  JSON.stringify({
+                    event: "interim-transcription",
+                    text: data.results[0].alternatives[0].transcript,
+                  })
+                );
               }
-            );
-
-            // Send the transcription data in the request body
-            streamingReq.write(
-              JSON.stringify({
-                text: data.results[0].alternatives[0].transcript,
-              })
-            );
-
-            streamingReq.end();
+            });
           });
         break;
       case "start":
@@ -103,29 +77,6 @@ wss.on("connection", function connection(ws) {
   });
 });
 
-app.post("/keywords", async (req, res) => {
-  try {
-    const { keywords } = req.body; // Extract the keywords from the request body
-    console.log(keywords);
-    const response = await axios.post(
-      "http://localhost:8000/similarity",
-      {
-        keywords: keywords,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    res.status(200).json(response.data); // Send the response data to the frontend
-  } catch (error) {
-    console.error(error); // Log any errors
-    res.sendStatus(500);
-  }
-});
-
 // Handle HTTP Request
 app.get("/", async (req, res) => {
   res.sendStatus(200);
@@ -137,7 +88,7 @@ app.post("/", (req, res) => {
   res.send(`
     <Response>
       <Start>
-        <Stream url="wss://${req.headers.host}/"/>
+        <Stream url="ws://geohilfe.eu-central-1.elasticbeanstalk.com"/>
       </Start>
       <Say>Emergency Fire and Rescue Services. Where exactly is the location of the emergency? </Say>
       <Pause length="30" />
@@ -145,8 +96,7 @@ app.post("/", (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 4000
-
+const PORT = process.env.PORT || 4000;
 
 console.log("Listening at Port", PORT);
 server.listen(PORT);
