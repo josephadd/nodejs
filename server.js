d0b5543e7ea8b5ecf091178be4df5cc9104a5090
nx-joseph-adeabah <85238365+josephadd@users.noreply.github.com>
2023-07-23 09:10:10 +0000

    updated

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.post("/", (req, res) => {
   res.send(`
     <Response>
       <Start>
-        <Stream url="wss://geohilfe.us-east-1.elasticbeanstalk.com/"/>
+        <Stream url="wss://geohilfe-api.com/"/>
       </Start>
       <Say>Emergency Fire and Rescue Services. Where exactly is the location of the emergency? </Say>
       <Pause length="30" />
