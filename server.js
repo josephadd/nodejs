6c387625a8f051dce53baff4811728e16bc70df4
nx-joseph-adeabah <85238365+josephadd@users.noreply.github.com>
2023-08-04 10:28:13 +0000

    fix: fixed google service env

--- server.js
+++ server.js
@@ -3,23 +3,36 @@ const express = require("express");
 const cors = require("cors");
 const http = require("http");
 const app = express();
-app.use(cors()); // Enable CORS for all routes
+app.use(cors()); 
 app.use(express.json());
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const speech = require("@google-cloud/speech");
 require("dotenv").config();
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
 
+const configFile = path.join(__dirname, 'routes/geohilfev1-1b4d84da44a9.json');
 const client = new speech.SpeechClient({
-  keyFilename: path.join(__dirname, 'routes/geohilfev1-1b4d84da44a9.json')
+  keyFilename: configFile,
 });
 
-// Configure Transcription Request
+const configData = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+
+configData.project_id = process.env.PROJECT_ID || configData.project_id;
+configData.private_key = process.env.PRIVATE_KEY || configData.private_key;
+configData.private_key_id = process.env.PRIVATE_KEY_ID || configData.private_key_id;
+configData.client_email = process.env.CLIENT_EMAIL || configData.client_email;
+configData.client_id = process.env.CLIENT_ID || configData.client_id;
+configData.auth_uri = process.env.AUTH_URI || configData.auth_uri;
+configData.token_uri = process.env.TOKEN_URI || configData.token_uri;
+configData.auth_provider = process.env.AUTH_PROVIDER || configData.auth_provider;
+configData.client_cert_url = process.env.CLIENT_CERT_URL || configData.client_cert_url;
+
 const request = {
   config: {
-    encoding: "MULAW", // Use FLAC encoding
-    sampleRateHertz: 8000, // Set the sample rate for FLAC audio (usually 16000 or 44100)
+    encoding: "MULAW", 
+    sampleRateHertz: 8000,
     languageCode: "en-GB",
     enableAutomaticPunctuation: true,
     enableWordConfidence: true,
@@ -84,7 +97,7 @@ app.post("/keywords", async (req, res) => {
     const { keywords } = req.body; // Extract the keywords from the request body
     console.log(keywords);
     const response = await axios.post(
-      "http://localhost:8000/similarity",
+      "https://geohilfe-api.com/similarity",
       {
         keywords: keywords,
       },
@@ -105,18 +118,16 @@ app.post("/keywords", async (req, res) => {
 // Handle Twilio Webhook
 app.post("/", (req, res) => {
   res.set("Content-Type", "text/xml");
-
-  // Extract the transcription text from the Twilio request
-
-  // You can process the transcription text here if needed
-
   // Respond with TwiML containing the next set of instructions
   res.send(`
     <Response>
-      <Start>
-        <Stream url="wss://geohilfe-api.com"/>
-      </Start>
+      <Gather action="wss://geohilfe-api.com/" method="GET" input="speech dtmf" finishOnKey="#" timeout="3" numDigits="1">
       <Say>Emergency Fire and Rescue Services. Where exactly is the location of the emergency? </Say>
+      </Gather>
+      <Start>
+      <Stream url="wss://geohilfe-api.com"/>
+    </Start>
+      <Say>We didn't receive any input. Goodbye!</Say>
       <Pause length="30" />
     </Response>
   `);
